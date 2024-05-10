afe0a6b48451b11a1cbe8e3bcdd36ac8111b72e1
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-05-10 14:38:57 +1000

    Added BannerImageCarousel

---
+++ src/lib/components/BannerImageCarousel.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Autoplay from 'embla-carousel-autoplay';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import * as Carousel from '$lib/components/ui/carousel/index.js';
+
+	export let delay = 3000;
+	export let images: { src: string; alt: string }[];
+	const plugin = Autoplay({ delay, stopOnInteraction: true });
+</script>
+
+<Carousel.Root
+	plugins={[plugin]}
+	class="w-full"
+	opts={{ loop: true }}
+	on:mousenter={plugin.stop}
+	on:mouseleave={plugin.reset}
+>
+	<Carousel.Content>
+		{#each images as { src, alt }}
+			<Carousel.Item>
+				<Card.Root>
+					<Card.Content class="flex items-center justify-center p-0">
+						<img {src} {alt} class="aspect-auto h-full w-full object-cover" />
+					</Card.Content>
+				</Card.Root>
+			</Carousel.Item>
+		{/each}
+	</Carousel.Content>
+	<!-- <Carousel.Previous />
+  <Carousel.Next /> -->
+</Carousel.Root>
+
+<style>
+	img {
+		width: 100%;
+		/* max-height: 100%;
+    max-width: 100%; */
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import BannerImageCarousel from "$components/BannerImageCarousel.svelte";
 </script>
 
 <svelte:head>
@@ -6,6 +7,7 @@
 	<meta name="description" content="Lens of Brown Photography" />
 </svelte:head>
 
+<BannerImageCarousel images={[{ src: 'https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg', alt: 'grassy meadow' }, { src: 'https://cdn.naturettl.com/wp-content/uploads/2020/04/25152036/how-to-find-great-locations-for-landscape-photography-11.jpg', alt: 'rocky beach'}, { src: 'https://d150u0abw3r906.cloudfront.net/wp-content/uploads/2021/10/image2-2-1024x649.png', alt: 'lake with snowy mountains backdrop' }, { src: 'https://vastphotos.com/files/uploads/photos/10691/peaceful-water-landscape-photo-l.jpg?v=20220712073521', alt: 'Road with snowy mountains backdrop' }]}/>
 <section class="flex flex-col m-auto">
 	<h1 class="justify-center text-4xl scroll-m-20 font-semibold">Launching soon...</h1>
 </section>
