d7d2c64c06694f9b08ce09074afcae279f61af37
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-05-21 04:47:39 +1000

    Added postcss

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,3 @@
-import '../app.css';
+import '../app.postcss';
 
 export const prerender = true;
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
 		adapter: adapter({
@@ -21,7 +27,7 @@ const config = {
 			// $icons: './src/lib/icons',
 			$stores: './src/lib/stores',
 			// $types: './src/lib/types',
-			$utils: './src/lib/utils',
+			$utils: './src/lib/utils'
 			// $constants: './src/lib/constants.ts'
 		}
 	}
