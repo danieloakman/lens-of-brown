b02a9734f64b31000f79cddaeafa06ae261e1493
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-05-22 21:09:26 +1000

    Added purgeCss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vitest/config';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 export default defineConfig({
 	plugins: [
@@ -8,7 +9,8 @@ export default defineConfig({
 		(Icons as any)({
 			compiler: 'svelte',
 			autoInstall: true
-		})
+		}),
+		purgeCss()
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
