52ea94c5c619ae1f68369eee2c03046f63fc28e5
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-06-06 18:18:43 +1000

    Carousel now works on large images

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	// Create svelte-5 prop
+	const windowSize: { w: number; h: number } = $state({ h: 0, w: 0 });
 
 	const {
 		buttons,
@@ -7,7 +7,7 @@
 		imgs
 	}: { buttons?: boolean; nextDelayMs?: number; imgs: { src: string; alt: string }[] } = $props();
 
-	let elemCarousel: HTMLDivElement;
+	let elemCarousel: HTMLDivElement | undefined;
 	const unsplashIds = [
 		'vjUokUWbFOs',
 		'1aJuPtQJX_I',
@@ -18,6 +18,7 @@
 	];
 
 	function carouselLeft(): void {
+		if (!elemCarousel) return;
 		const x =
 			elemCarousel.scrollLeft === 0
 				? elemCarousel.clientWidth * elemCarousel.childElementCount // loop
@@ -26,6 +27,7 @@
 	}
 
 	function carouselRight(): void {
+		if (!elemCarousel) return;
 		const x =
 			elemCarousel.scrollLeft === elemCarousel.scrollWidth - elemCarousel.clientWidth
 				? 0 // loop
@@ -34,7 +36,7 @@
 	}
 
 	function carouselThumbnail(index: number) {
-		elemCarousel.scroll(elemCarousel.clientWidth * index, 0);
+		elemCarousel?.scroll(elemCarousel.clientWidth * index, 0);
 	}
 
 	setInterval(() => {
@@ -42,30 +44,35 @@
 	}, nextDelayMs);
 </script>
 
-<div class="card grid grid-cols-[auto_1fr_auto] items-center">
-	<!-- Button: Left -->
-	{#if buttons}
-		<button type="button" class="btn-icon variant-filled" onclick={carouselLeft}>
-			<i class="fa-solid fa-arrow-left"></i>
-		</button>
-	{/if}
+<svelte:window bind:innerWidth={windowSize.w} bind:innerHeight={windowSize.h} />
 
+<div class="grid grid-cols-[auto_1fr_auto] items-center">
+	<!-- Button: Left -->
+	<!-- <button type="button" class="btn-icon variant-filled" on:click={carouselLeft}>
+		<i class="fa-solid fa-arrow-left" />
+	</button> -->
 	<!-- Full Images -->
 	<div bind:this={elemCarousel} class="snap-x snap-mandatory scroll-smooth flex overflow-x-auto">
-		{#each unsplashIds as unsplashId}
+		{#each imgs as { src, alt }}
 			<img
-				class="snap-center w-full object-cover"
+			  class="snap-center object-scale-down"
+				{src}
+				{alt}
+				height={windowSize.h}
+			/>
+		{/each}
+		<!-- Working example, but doesn't stretch the image -->
+		<!-- {#each unsplashIds as unsplashId}
+			<img
+				class="snap-center w-[1024px] rounded-container-token"
 				src="https://source.unsplash.com/{unsplashId}/1024x768"
 				alt={unsplashId}
 				loading="lazy"
 			/>
-		{/each}
+		{/each} -->
 	</div>
-
 	<!-- Button: Right -->
-	{#if buttons}
-		<button type="button" class="btn-icon variant-filled" onclick={carouselRight}>
-			<i class="fa-solid fa-arrow-right"></i>
-		</button>
-	{/if}
+	<!-- <button type="button" class="btn-icon variant-filled" on:click={carouselRight}>
+		<i class="fa-solid fa-arrow-right" />
+	</button> -->
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,30 @@
 	import Carousel from '$components/Carousel.svelte';
 </script>
 
-<Carousel imgs={[]}/>
+<Carousel
+	imgs={[
+		{
+			src: 'https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?cs=srgb&dl=pexels-bri-schneiter-28802-346529.jpg&fm=jpg',
+			alt: 'A beautiful lake'
+		},
+		{
+			src: 'https://images.pexels.com/photos/1107717/pexels-photo-1107717.jpeg?cs=srgb&dl=pexels-fotios-photos-1107717.jpg&fm=jpg',
+			alt: 'A beautiful green plain by the water'
+		},
+		// {
+		// 	src: 'https://thecamerastore.com/cdn/shop/articles/GINAYEOLANDSCAPE-1_800x.jpg?v=1687296669',
+		// 	alt: 'A beautiful mountain with pine trees and a river'
+		// },
+		// {
+		// 	src: 'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg?w=1436&h=1078',
+		// 	alt: 'A road and sunset'
+		// },
+		{
+			src: 'https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography.jpg',
+			alt: 'A desert'
+		}
+	]}
+/>
 
 <h1 class="text-5xl p-10">Hi</h1>
 <h1 class="text-5xl p-10">Hi</h1>
