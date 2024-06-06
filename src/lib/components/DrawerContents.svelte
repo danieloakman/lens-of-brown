14a20ef36142d6f4da1981ba5bc81b546946c839
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-06-06 18:20:01 +1000

    Removed old stuff from DrawerContents

--- src/lib/components/DrawerContents.svelte
+++ src/lib/components/DrawerContents.svelte
@@ -7,23 +7,7 @@
 
 {#if $drawerStore.open}
 	<div class="m-4 flex justify-between items-center">
-		<!-- <button class="btn-icon variant-filled-primary" on:click={family.reload}>
-			<RefreshIcon />
-		</button> -->
-
 		<LightSwitch />
-
-		<!-- <Avatar src={$currentUserAvatar} /> -->
-
-		<!-- <button
-			class="btn-icon variant-filled-primary"
-			on:click={() => {
-				logout();
-				drawerStore.close();
-			}}
-		>
-			<LogoutIcon />
-		</button> -->
 	</div>
 	<hr />
 
