a93b93ef2f323f6a32d46fbb52ba391ba3fdfb3d
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-06-07 07:44:52 +1000

    Removed unused import

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { Icon, routes } from '$lib';
-	import { AppBar, LightSwitch, getDrawerStore } from '@skeletonlabs/skeleton';
+	import { AppBar, getDrawerStore } from '@skeletonlabs/skeleton';
 	import { headerStore } from '$stores/ui';
 
 	function canGoBack(routes: string[]): boolean {
