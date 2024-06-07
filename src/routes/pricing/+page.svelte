3b1c40c6ff189519330f9f8479a2e26c9a3dd524
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-06-07 16:29:21 +1000

    Some stuff to pricing

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -1,1 +1,12 @@
-<span>Pricing!</span>
+<script lang="ts">
+</script>
+
+<div class="flex flex-col items-center p-4">
+	<h1 class="text-5xl text-primary-500 dark:text-primary-100">PRICE LIST</h1>
+</div>
+
+<style lang="postcss">
+	h1 {
+		@apply italic;
+	}
+</style>
