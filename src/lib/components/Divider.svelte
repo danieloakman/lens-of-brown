e2c6c89be63315352868271d3cd0be0abdcb4a26
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-06-11 10:30:43 +1000

    More work on /pricing

--- src/lib/components/Divider.svelte
+++ src/lib/components/Divider.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
+	import { omit } from 'js-utils';
 	const {
-		horizontal = true,
+		horizontal = false,
 		vertical = false,
-		width = 2
-	}: { vertical?: boolean; horizontal?: boolean; width?: number } = $props();
+		width = 2,
+		...rest
+	}: { vertical?: boolean; horizontal?: boolean; width?: number; class?: string } = $props();
 </script>
 
 {#if horizontal}
-  <hr style={`border-top-width:${width}px`}/>
+	<hr style={`border-top-width:${width}px`} {...rest} />
 {:else if vertical}
-	<span class="divider-vertical" style={`height:${width}px`}></span>
+<!-- TODO: Fix width: -->
+	<span
+		class={'divider-vertical w-full' + rest.class}
+		{...omit(rest, 'class')}
+	>
+	</span>
+{:else}
+	<span>Did not specify type of divider!</span>
 {/if}
--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -1,85 +1,77 @@
 <script lang="ts">
-	import Divider from "$components/Divider.svelte";
-	import PriceList from "./PriceList.svelte";
+	import Divider from '$components/Divider.svelte';
 
+	type PricePackage = [name: string, amount: number];
+	const miniShoots: PricePackage[] = [
+		['FAMILY', 150],
+		['COUPLES', 150],
+		['ENGAGEMENT', 150],
+		['SOLO', 100],
+		['PETS', 100]
+	];
+	const packages: PricePackage[] = [
+		['FAMILY OR SIBLINGS', 300],
+		['COUPLES', 300],
+		['PROPOSAL OR ENGAGEMENT', 300],
+		['WEDDINGS OR ELOPEMENTS', 700],
+		['EVENTS', 450]
+	];
 </script>
 
-<div class="flex flex-col pb-8">
-	<h1 class="text-5xl text-primary-500 dark:text-primary-100 mx-auto py-10">
+<div class="flex flex-col pb-8 gap-10">
+	<h1 class="text-primary-500 dark:text-primary-100 mx-auto pt-10">
 		PRICE &nbsp;&nbsp; LIST*
 	</h1>
 
-	<Divider horizontal width={2}/>
+	<Divider horizontal />
 
-	<h2 class="text-3xl mx-auto">MINI-SHOOTS (10 EDITED IMAGES)</h2>
+	<div class="flex flex-col mx-auto gap-6">
+		<div class="flex flex-col gap-6">
+			<h2>MINI-SHOOTS (10 EDITED IMAGES)</h2>
 
-	<div>
-		<!-- <PriceList prices={[['FAMILY', 150]]} class="mx-auto px-10 py-5"/> -->
-		<div>
-			<span>FAMILY</span>
-			<span>$150</span>
+			<div class="flex flex-col w-full px-6 gap-4">
+				{#each miniShoots as [name, amount]}
+					<div class="flex flex-row justify-between">
+						<span class="text-lg">{name}</span>
+						<span class="text-lg"><b>$</b>{amount}</span>
+					</div>
+				{/each}
+			</div>
 		</div>
 
-		<div>
-			<span>COUPLES</span>
-			<span>$150</span>
-		</div>
+		<div class="flex flex-col gap-6">
+			<div class="flex flex-row justify-between items-baseline">
+				<h2>PACKAGES</h2>
+				<span>STARTS FROM:</span>
+			</div>
 
-		<div>
-			<span>ENGAGEMENT</span>
-			<span>$150</span>
-		</div>
-
-		<div>
-			<span>SOLO</span>
-			<span>$100</span>
-		</div>
-
-		<div>
-			<span>PETS</span>
-			<span>$100</span>
+			<div class="flex flex-col w-full px-6 gap-4">
+				{#each packages as [name, amount]}
+					<div class="flex flex-row justify-between">
+						<span class="text-lg">{name}</span>
+						<span class="text-lg"><b>$</b>{amount}</span>
+					</div>
+				{/each}
+			</div>
 		</div>
 	</div>
 
-	<div class="flex flex-row justify-between">
-		<h2>PACKAGES</h2>
-		<span>STARTS FROM:</span>
-	</div>
-
-	<div>
-		<div>
-			<span>FAMILY OR SIBLINGS</span>
-			<span>$300</span>
-		</div>
-
-		<div>
-			<span>COUPLES</span>
-			<span>$300</span>
-		</div>
-
-		<div>
-			<span>PROPOSAL OR ENGAGEMENT</span>
-			<span>$300</span>
-		</div>
-
-		<div>
-			<span>WEDDINGS OR ELOPEMENTS</span>
-			<span>$700</span>
-		</div>
-
-		<div>
-			<span>EVENTS</span>
-			<span>$450</span>
-		</div>
+	<div class="mx-auto p-6 card flex flex-row justify-between gap-2">
+		<span>LENS.OFBROWN</span>
+		<Divider vertical class="text-success-500 w-full" width={10} />
+		<span>INFO@LENSOFBROWN.COM.AU</span>
 	</div>
 
 	<Divider horizontal />
 
-  <h3 class="mx-auto">*PRICES SUBJECT TO CHANGE AT ANY TIME</h3>
+	<h3 class="mx-auto">*PRICES SUBJECT TO CHANGE</h3>
 </div>
 
 <style lang="postcss">
 	h1 {
-		/* @apply italic; */
+		@apply text-5xl
+	}
+	h2 {
+		@apply text-3xl
 	}
 </style>
