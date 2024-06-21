0951d4ab019059cb3ea3f09b50fe32dd53fe94cb
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-06-21 18:10:39 +1000

    Added some images to portfolio

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,18 +1,40 @@
 <script lang="ts">
-	import ImgCarousel from '$lib/components/ImgCarousel.svelte';
 	import { STATIC_FILES } from '$lib';
-	import { iter } from 'iteragain';
+	import { iter, take } from 'iteragain';
 
-	const randImgs = iter(Object.values(STATIC_FILES.imgs.clients)).map(
-		(src) =>
-			({
-				src,
-				alt: 'an img'
-			}) as { src: string; alt: string }
+	function random(seed: number) {
+		var x = Math.sin(seed++) * 10000;
+		return x - Math.floor(x);
+	}
+	function shuffle<T>(array: T[], seed: number): T[] {
+		// TODO: refactor this
+		var m = array.length,
+			t,
+			i;
+
+		// While there remain elements to shuffle…
+		while (m) {
+			// Pick a remaining element…
+			i = Math.floor(random(seed) * m--);
+
+			// And swap it with the current element.
+			t = array[m];
+			array[m] = array[i] as T;
+			array[i] = t as T;
+			++seed;
+		}
+
+		return array;
+	}
+	const seed = 5;
+	const imgs = take(
+		shuffle(iter(Object.values(STATIC_FILES.imgs.clients) as string[]).toArray(), seed),
+		10
 	);
 </script>
 
-<div class="p-4 grid grid-cols-2 gap-4">
-	<ImgCarousel class="" nextDelayMs={3000} imgs={randImgs.take(3)} />
-	<ImgCarousel class="" nextDelayMs={5000} imgs={randImgs.take(3)} />
+<div class="p-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+	{#each imgs as img}
+		<img alt="Portfolio image for lens of brown" src={img} />
+	{/each}
 </div>
