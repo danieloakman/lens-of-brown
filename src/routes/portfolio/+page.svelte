a7dda413a2b9cd8a80834fe5cd68c78050cc3c8f
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-06-22 23:14:24 +1000

    Some more work towards portfolio

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { STATIC_FILES } from '$lib';
 	import { iter, take } from 'iteragain';
+	import TwoImg from './TwoImg.svelte';
 
 	function random(seed: number) {
 		var x = Math.sin(seed++) * 10000;
@@ -57,20 +58,31 @@
 	Title below image square, with subheading
 	 -->
 	<div class="py-4 px-4 mx-auto md:px-52 grid grid-cols-1 md:grid-cols-2 gap-36">
-		{#each imgs as img}
-			<img
+			<div class="flex flex-col">
+				<TwoImg class=""
+					imgs={[
+						{ src: STATIC_FILES.imgs.clients['P5151059.JPG'], alt: '' },
+						{ src: STATIC_FILES.imgs.clients['P5152015.JPG'], alt: '' }
+					]}
+				/>
+				<h2>Family</h2>
+			</div>
+			<!-- <img
 				alt="Portfolio for lens of brown"
 				src={img}
 				loading="lazy"
 				class="w-[400px] object-cover aspect-square rounded-lg hover-sm"
-			/>
-		{/each}
+			/> -->
 	</div>
 </div>
 
 <style lang="postcss">
 	h1 {
 		@apply text-5xl mx-auto;
+	}
+
+	h2 {
+		@apply text-3xl text-center;
 	}
 
 	.hover-sm {
---
+++ src/routes/portfolio/TwoImg.svelte
@@ -0,0 +1,24 @@
+<!-- Displays one image, then on hover or click, display another. -->
+<script lang="ts">
+	import { omit } from 'js-utils';
+	import { quintOut } from 'svelte/easing';
+	import type { HTMLImgAttributes } from 'svelte/elements';
+	import { crossfade } from 'svelte/transition';
+
+	interface Img {
+		src: string;
+		alt: string;
+	}
+	const { imgs, ...rest }: Omit<HTMLImgAttributes, 'src' | 'alt'> & { imgs: [Img, Img] } = $props();
+
+	let i = $state(0);
+	const img = $derived(imgs[i]);
+
+	// const [send, receive] = crossfade({
+	// 	duration: 500,
+	// 	easing: quintOut
+	// });
+</script>
+
+<img class={'absolute object-cover ' + rest.class} {...img} {...omit(rest, 'class')} />
+<img class={'absolute object-cover' + rest.class} {...img} {...omit(rest, 'class')} />
