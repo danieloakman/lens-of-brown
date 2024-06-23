00085f2d41062ff7bc6893f5bce867d7a82c2b92
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-06-24 07:11:31 +1000

    Some work towards portfolio

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -38,7 +38,7 @@
 	<h1 class="p-4 text-center">See what we've been up to!</h1>
 
 	<!-- Sections in grid: -->
-	<h1>Family</h1>
+	<!-- <h1>Family</h1>
 
 	<h1>Couples</h1>
 
@@ -48,7 +48,7 @@
 
 	<h1>Portraits</h1>
 
-	<h1>Landscape</h1>
+	<h1>Landscape</h1> -->
 
 	<!-- TODO once we have pics of weddings <h1>Weddings</h1> -->
 
@@ -57,17 +57,42 @@
 	2x2 on desktop, 1x4 on mobile
 	Title below image square, with subheading
 	 -->
-	<div class="py-4 px-4 mx-auto md:px-52 grid grid-cols-1 md:grid-cols-2 gap-36">
-			<div class="flex flex-col">
-				<TwoImg class=""
-					imgs={[
-						{ src: STATIC_FILES.imgs.clients['P5151059.JPG'], alt: '' },
-						{ src: STATIC_FILES.imgs.clients['P5152015.JPG'], alt: '' }
-					]}
-				/>
-				<h2>Family</h2>
-			</div>
-			<!-- <img
+	<div class="py-4 px-4 mx-auto md:px-52 grid grid-cols-1 md:grid-cols-2 gap-32">
+		<div class="flex flex-col gap-4">
+			<TwoImg
+				class="rounded-lg w-[400px] aspect-square object-cover"
+				imgs={[
+					{
+						src: STATIC_FILES.imgs.clients['P5151059.JPG'],
+						alt: 'Family of 4 amongst Autumn leaves.'
+					},
+					{
+						src: STATIC_FILES.imgs.clients['P5152015.JPG'],
+						alt: 'Boy and girl toddlers playing with Autumn leaves.'
+					}
+				]}
+			/>
+			<h2>Family</h2>
+		</div>
+
+		<div class="flex flex-col gap-4">
+			<TwoImg
+				class="rounded-lg w-[400px] aspect-square object-cover"
+				loading="lazy"
+				imgs={[
+					{
+						src: STATIC_FILES.imgs.clients['P6020120.jpg'],
+						alt: 'Family of 4 amongst Autumn leaves.'
+					},
+					{
+						src: STATIC_FILES.imgs.clients['P6020330.jpg'],
+						alt: 'Boy and girl toddlers playing with Autumn leaves.'
+					}
+				]}
+			/>
+			<h2>Animals</h2>
+		</div>
+		<!-- <img
 				alt="Portfolio for lens of brown"
 				src={img}
 				loading="lazy"
@@ -82,10 +107,10 @@
 	}
 
 	h2 {
-		@apply text-3xl text-center;
+		@apply text-4xl text-center;
 	}
 
-	.hover-sm {
-		@apply transition duration-300 ease-in-out hover:scale-105;
+	h3 {
+		@apply text-3xl text-center;
 	}
 </style>
