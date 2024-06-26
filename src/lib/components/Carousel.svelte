73d172e55866ca136d091d8748d66115fa5b91e7
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-06-27 06:24:07 +1000

    Simplified Carousel a bit

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -4,19 +4,19 @@
 
 	const {
 		buttons,
-		nextDelayMs = 5000,
 		children,
-		class: klass = ''
+		class: klass = '',
+		innerClass = ''
 	}: {
 		buttons?: boolean;
-		nextDelayMs?: number;
 		children: Snippet;
 		class?: string;
+		innerClass?: string;
 	} = $props();
 
 	let elemCarousel: HTMLDivElement | undefined;
 
-	function carouselLeft(): void {
+	export function left(): void {
 		if (!elemCarousel) return;
 		const x =
 			elemCarousel.scrollLeft === 0
@@ -25,7 +25,7 @@
 		elemCarousel.scroll(x, 0);
 	}
 
-	function carouselRight(): void {
+	export function right(): void {
 		if (!elemCarousel) return;
 		const x =
 			elemCarousel.scrollLeft === elemCarousel.scrollWidth - elemCarousel.clientWidth
@@ -33,43 +33,26 @@
 				: elemCarousel.scrollLeft + elemCarousel.clientWidth; // step right
 		elemCarousel.scroll(x, 0);
 	}
-
-	// function carouselThumbnail(index: number) {
-	// 	elemCarousel?.scroll(elemCarousel.clientWidth * index, 0);
-	// }
-
-	setInterval(() => {
-		carouselRight();
-	}, nextDelayMs);
 </script>
 
 <div class={'grid grid-cols-[auto_1fr_auto] items-center ' + klass}>
 	{#if buttons}
-		<button type="button" class="btn-icon variant-filled" onclick={carouselLeft}>
+		<button type="button" class="btn-icon variant-filled-primary mr-2" onclick={left}>
 			<Icon.ArrowLeft />
 		</button>
 	{/if}
 
-	<div bind:this={elemCarousel} class="snap-x snap-mandatory scroll-smooth flex overflow-x-auto">
+	<div
+		bind:this={elemCarousel}
+		class={'snap-x snap-mandatory scroll-smooth flex overflow-x-auto ' + innerClass}
+	>
 		{#if children}
 			{@render children()}
 		{/if}
-		<!-- {#each imgs as { src, alt }}
-			<img class={"snap-center " + imgClass} {src} {alt} />
-		{/each} -->
-		<!-- Working example, but doesn't stretch the image -->
-		<!-- {#each unsplashIds as unsplashId}
-			<img
-				class="snap-center w-[1024px] rounded-container-token"
-				src="https://source.unsplash.com/{unsplashId}/1024x768"
-				alt={unsplashId}
-				loading="lazy"
-			/>
-		{/each} -->
 	</div>
 
 	{#if buttons}
-		<button type="button" class="btn-icon variant-filled-primary" onclick={carouselRight}>
+		<button type="button" class="btn-icon variant-filled-primary ml-2" onclick={right}>
 			<Icon.ArrowRight />
 		</button>
 	{/if}
--- src/routes/portfolio/families/+page.svelte
+++ src/routes/portfolio/families/+page.svelte
@@ -2,16 +2,30 @@
 	import Carousel from '$components/Carousel.svelte';
 	import Img1 from '$imgs/clients/P5190160.jpg?aspect=1:1&w=700&webp&imagetools';
 	import Img2 from '$imgs/clients/P5190601.jpg?aspect=1:1&w=700&webp&imagetools';
+
+	let carousel: Carousel | null = null;
+	$effect(() => {
+		console.log(carousel);
+		setInterval(() => {
+			carousel?.right();
+		}, 5000);
+	});
 </script>
 
-<h1 class="text-5xl text-center">Families</h1>
-<Carousel class="w-[600px] mx-auto">
-	<img src={Img1} alt="Family of 4 amongst Autumn leaves" />
-	<img src={Img2} alt="todo" />
-</Carousel>
+<div class="p-4 flex flex-col gap-4">
+	<h1 class="text-5xl text-center">Families</h1>
+	<Carousel
+		bind:this={carousel}
+		class="w-[600px] mx-auto rounded-container-token"
+		innerClass="rounded-container-token"
+	>
+		<img src={Img1} alt="Family of 4 amongst Autumn leaves" />
+		<img src={Img2} alt="todo" />
+	</Carousel>
+</div>
 
 <style lang="postcss">
 	img {
-		@apply snap-center rounded-container-token aspect-square object-cover w-[600px];
+		@apply snap-center aspect-square object-cover w-[600px];
 	}
 </style>
