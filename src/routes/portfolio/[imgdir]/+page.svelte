71d4d878f4e47f611afd51a3f841fc24972bc891
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-07-03 06:19:00 +1000

    Swapped to eager false

--- src/routes/portfolio/[imgdir]/+page.svelte
+++ src/routes/portfolio/[imgdir]/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { iife } from 'js-utils';
+	import { iife, propIs } from 'js-utils';
 	import { iter, repeat } from 'iteragain';
+
+	const assertImg = (obj: unknown): { default: string } => {
+		if (propIs(obj, 'default', 'object') || propIs(obj, 'default', 'string')) return obj as unknown as { default: string };
+		throw new Error('Expected object with default string property');
+	};
 
 	type ImgDir =
 		| 'portraits'
@@ -14,12 +19,12 @@
 
 	const imgdir = $page.params.imgdir as ImgDir | unknown;
 	let alts: IterableIterator<string> = repeat('todo', 12);
-	const imgs: Record<string, { default: string }> = iife(() => {
+	const imgs: Record<string, () => Promise<unknown>> = iife(() => {
 		// **The following non-dynamic if statements are required for vite to correctly resolve the glob.**
 		// To add more image directories, add more if statements.
 		if (imgdir === 'portraits') {
 			return import.meta.glob('$imgs/portraits/*.{jpg,jpeg,png,webp}', {
-				eager: true,
+				eager: false,
 				query: {
 					enhanced: true
 				}
@@ -27,75 +32,61 @@
 		}
 		if (imgdir === 'families')
 			return import.meta.glob('$imgs/families/*.{jpg,jpeg,png,webp}', {
-				eager: true,
+				eager: false,
 				query: {
 					enhanced: true
 				}
 			});
 		if (imgdir === 'couples')
 			return import.meta.glob('$imgs/couples/*.{jpg,jpeg,png,webp}', {
-				eager: true,
+				eager: false,
 				query: {
 					enhanced: true
 				}
 			});
 		if (imgdir === 'landscapes')
 			return import.meta.glob('$imgs/landscapes/*.{jpg,jpeg,png,webp}', {
-				eager: true,
+				eager: false,
 				query: {
 					enhanced: true
 				}
 			});
 		if (imgdir === 'weddings')
 			return import.meta.glob('$imgs/weddings/*.{jpg,jpeg,png,webp}', {
-				eager: true,
+				eager: false,
 				query: {
 					enhanced: true
 				}
 			});
 		if (imgdir === 'events')
 			return import.meta.glob('$imgs/events/*.{jpg,jpeg,png,webp}', {
-				eager: true,
+				eager: false,
 				query: {
 					enhanced: true
 				}
 			});
 		if (imgdir === 'animals')
 			return import.meta.glob('$imgs/animals/*.{jpg,jpeg,png,webp}', {
-				eager: true,
+				eager: false,
 				query: {
 					enhanced: true
 				}
 			});
-		// if (dev && imgdir === 'test') {
-		// 	const imgs = import.meta.glob('$imgs/wombat*', {
-		// 		eager: true,
-		// 		query: {
-		// 			enhanced: true,
-		// 		}
-		// 	});
-		//   const img = Object.values(imgs)[0];
-		//   if (!(isObjectLike(img) && ('default' in img))) throw new Error('test img.default is not a string');
-		// 	return iter(repeat(img as { default: string }, 12))
-		// 		.enumerate()
-		// 		.reduce(
-		// 			(a, [i, v]) => ((a[i.toString()] = v), a),
-		// 			{} as Record<string, { default: string }>
-		// 		);
-		// }
 		throw new Error(`Unknown imgdir: ${imgdir}`);
 	});
 </script>
 
 <div class="grid grid-cols-2 md:grid-cols-3 gap-4 p-4">
-	{#each iter(Object.values(imgs)).pluck('default').zip(alts) as [src, alt]}
-		<enhanced:img
-			{src}
-			{alt}
-			class="rounded-container-token w-[400px] md:w-[700px] object-cover"
-			style="aspect-ratio: 0.67/1;"
-			sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px"
-		></enhanced:img>
+	{#each iter(Object.values(imgs)).zip(alts) as [pSrc, alt]}
+		{#await pSrc() then src}
+			<enhanced:img
+				src={assertImg(src).default}
+				{alt}
+				class="rounded-container-token w-[400px] md:w-[700px] object-cover"
+				style="aspect-ratio: 0.67/1;"
+				sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px"
+			></enhanced:img>
+		{/await}
 	{/each}
 </div>
 
