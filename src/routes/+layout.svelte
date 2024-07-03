f20115988f4eb5a0a34f60b5036bc8e8433a9abb
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-07-03 12:54:58 +1000

    Use 33% width for drawer when in sm breakpoint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -40,7 +40,7 @@
 <Toast />
 <Drawer
 	position="right"
-	width="w-[200px] sm:w-[400px]"
+	width="w-[200px] sm:w-[33%]"
 	padding="p-0"
 	rounded="rounded-none"
 	bgBackdrop="bg-gradient-to-tr from-primary-500/50 via-tertiary-500/50 to-secondary-500/50"
