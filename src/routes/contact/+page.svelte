efe53a1a201965fcd17222eb5c331c18cc45e244
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-07-16 06:37:42 +1000

    Some initial work on /contact

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,7 +1,42 @@
-<span>contact!</span>
+<script lang="ts">
+	import BottomBanner from '$components/BottomBanner.svelte';
+	import PageBody from '$components/PageBody.svelte';
+	import Input from '$components/Input.svelte';
+</script>
 
 <!--
 TODO: Copy over as is from zenfolio. But:
 Replace map pic with landscape pic.
-
 -->
+
+<PageBody>
+	<h1 class="text-5xl text-primary-500">Contact us</h1>
+	<form class="grid grid-cols-2 gap-x-4 gap-y-10">
+		<Input name="first_name" label="First name" placeholder="First name" required type="text" />
+		<Input name="last_name" label="Last name" placeholder="Last name" required type="text" />
+		<Input
+			class="col-span-2"
+			name="email"
+			label="Email"
+			placeholder="Your e-mail"
+			required
+			type="text"
+		/>
+		<Input class="col-span-2" name="phone_no" label="Phone number" type="tel" required />
+		<Input
+			name="shoot_type"
+			label="What kind of shoot type are you interested in?"
+			required
+			type="select"
+      options={[
+        ['portrait', 'Portrait'],
+        ['landscape', 'Landscape'],
+        ['event', 'Event'],
+        ['product', 'Product'],
+        ['other', 'Other'],
+      ]}
+		/>
+	</form>
+</PageBody>
+
+<BottomBanner />
