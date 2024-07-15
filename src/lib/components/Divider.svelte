64f943ab92c51e6ccff7a4ec7ec7dd33e5ac19cd
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-07-16 04:08:50 +1000

    Fixed class assignment in Divider

--- src/lib/components/Divider.svelte
+++ src/lib/components/Divider.svelte
@@ -4,17 +4,16 @@
 		horizontal = false,
 		vertical = false,
 		width = 2,
-		...rest
+		class: klass = '',
 	}: { vertical?: boolean; horizontal?: boolean; width?: number; class?: string } = $props();
 </script>
 
 {#if horizontal}
-	<hr style={`border-top-width:${width}px`} {...rest} />
+	<hr style={`border-top-width:${width}px`} class={klass} />
 {:else if vertical}
 	<span
-		class={'divider-vertical w-full' + rest.class}
+		class={'divider-vertical w-full ' + klass}
 		style={`border-left-width:${width}px`}
-		{...omit(rest, 'class')}
 	>
 	</span>
 {:else}
