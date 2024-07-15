99a6d0199011aa28c4bf992ec9c6b9ed031d78f8
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-07-16 07:11:56 +1000

    More placeholder stuff for /contact

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,6 +3,7 @@
 	import PageBody from '$components/PageBody.svelte';
 	import Input from '$components/Input.svelte';
 	import ForMore from '$components/ForMore.svelte';
+	import Divider from '$components/Divider.svelte';
 </script>
 
 <!--
@@ -38,6 +39,14 @@ Replace map pic with landscape pic.
 			]}
 		/>
 	</form>
+
+	<Divider horizontal class="w-[95%] max-w-[750px] mx-auto" />
+
+	<div>
+		
+	</div>
+
+	<Divider horizontal class="w-[95%] max-w-[750px] mx-auto" />
 
 	<ForMore />
 </PageBody>
