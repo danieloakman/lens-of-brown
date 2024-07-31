3de94e56dd5908df60aef0e040a10e3c861b1fa4
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-07-31 20:42:25 +1000

    Added OverlayChildren component

---
+++ src/lib/components/OverlayChildren.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	const { class: klass = '', children, ...rest }: HTMLAttributes<HTMLDivElement> = $props();
+</script>
+
+<div class={'container ' + klass} {...rest}>
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
+
+<style lang="postcss">
+	.container {
+		display: grid;
+		place-items: center;
+		grid-template-areas: 'inner-div';
+	}
+
+	.container > :global(*) {
+		grid-area: inner-div;
+	}
+</style>
