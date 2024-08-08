cd7a082d575abd926517d2322e58f019ffc8729b
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-08-09 06:43:49 +1000

    Remove unused imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,28 +5,18 @@
 	import {
 		AppShell,
 		Drawer,
-		Modal,
-		Toast,
+		// Modal,
+		// Toast,
 		initializeStores
 		// setInitialClassState
 	} from '@skeletonlabs/skeleton';
 	initializeStores();
 
 	import Header from '$components/Header.svelte';
-	// import Footer from '$components/Footer.svelte';
 	import DrawerContents from '$components/DrawerContents.svelte';
 
 	import { windowSize } from '$stores';
 	import { page } from '$app/stores';
-	import type { Action } from 'svelte/action';
-
-	// import { browser } from '$app/environment';
-	// TODO: get this working
-	// page.subscribe(() => {
-	// 	if (browser) {
-	// 		window?.scrollTo(0, 0);
-	// 	}
-	// });
 </script>
 
 <svelte:window bind:innerWidth={windowSize.w} bind:innerHeight={windowSize.h} />
