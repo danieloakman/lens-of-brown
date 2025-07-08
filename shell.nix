c9e0a46dbd2ac61ccb66f47832bedb673298a712
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-07-08 19:37:06 +0000

    Add a shell.nix file

---
+++ shell.nix
@@ -0,0 +1,36 @@
+{ pkgs ? import <nixpkgs> {} }:
+(pkgs.buildFHSEnv {
+  name = "lens-of-brown";
+  targetPkgs = pkgs: (with pkgs; [
+    # Required for node sharp:
+    swig
+    glibc
+    glib.dev
+    libffi
+    ffmpeg
+    libsmf
+    libGL
+    libz
+    libzip
+    libgcc
+    zlib
+    pango
+    fontconfig
+    cmake
+    pixman
+    cairo
+    libjpeg
+    giflib
+    librsvg
+    pkg-config
+
+    instaloader # Needed for downloading recent posts for lens.ofbrown
+  ]) ++ (with pkgs.xorg; [
+    libX11
+    libXext
+    libSM
+  ]);
+  # profile = ''
+  # '';
+  runScript = "zsh";
+}).env
